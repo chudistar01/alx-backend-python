e9b336fd942b8568fc5353f519a96569d0216523
chudistar01 <christianigwebuike31@gmail.com>
2024-09-14 16:25:29 +0000

    unittest

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""test module
+"""
+import unittest
+from typing import Dict, Tuple, Union
+from unittest.mock import patch, Mock
+from parameterized import parameterized
+
+from utils import access_nested_map
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """test case for multiple functions
+    """
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+        ])
+    def test_access_nested_map(self, nested_map: Dict, path: Tuple[str],
+                               expected: Union[Dict, int],
+                               ) -> None:
+        """test case for access_nested_map"""
+        self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    
