97835e4f48de4bcb60ed17662ab57c12dd181f6a
chudistar01 <christianigwebuike31@gmail.com>
2024-09-28 15:44:44 +0000

    Variable Annotations

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 """ Duck typing - first element """
-from typing import Tuple, List, Union
+from typing import Tuple, List, Any
 
 
-def zoom_array(lst: Tuple[int, ...], factor: int = 2) -> List:
+def zoom_array(lst: Tuple[Any, ...], factor: int = 2) -> List:
     """Zoom array"""
     zoomed_in: List = [
         item for item in lst
-        for i in range(factor)
+        for i in range(int(factor))
     ]
     return zoomed_in
 
